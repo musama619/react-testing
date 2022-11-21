ee144713f7622c84094df71fb34a25a2060dca49
musama619 <musama6191@gmail.com>
2022-11-21 21:50:57 +0500

    only, skip and grouping tests using describe

--- src/components/Greet.test.tsx
+++ src/components/Greet.test.tsx
@@ -1,8 +1,20 @@
 import { render, screen } from "@testing-library/react";
 import { Greet } from "./Greet";
-test('Greet renders correctly', () => {
-    render(<Greet />)
-    // const textElem = screen.getByText('Greet')
-    const textElem = screen.getByText(/greet/i) // regex to ignore case 
-    expect(textElem).toBeInTheDocument()
+
+
+describe('Greet', () => {
+    test.only('renders correctly', () => {
+        render(<Greet />)
+        // const textElem = screen.getByText('Greet')
+        const textElem = screen.getByText(/greet/i) // regex to ignore case 
+        expect(textElem).toBeInTheDocument()
+    })
+
+    describe('Nested', () => {
+        test.skip('renders correctly with name', () => {
+            render(<Greet name='Gon' />)
+            const textElem = screen.getByText('Greet Gon')
+            expect(textElem).toBeInTheDocument()
+        })
+    })
 })
