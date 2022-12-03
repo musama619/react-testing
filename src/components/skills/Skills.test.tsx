4981c0a7103646dbb9d30f5ca76109ba5770a54b
musama619 <musama6191@gmail.com>
2022-12-03 19:12:47 +0500

    queryBy

---
+++ src/components/skills/Skills.test.tsx
@@ -0,0 +1,44 @@
+import { render, screen } from '@testing-library/react'
+import { Skills } from './Skills'
+
+describe('Skills', () => {
+  const skills = ['HTML', 'CSS', 'JavaScript']
+  test('renders correctly', () => {
+    render(<Skills skills={skills} />)
+    const listElement = screen.getByRole('list')
+    expect(listElement).toBeInTheDocument()
+  })
+
+  test('renders a list of skills', () => {
+    render(<Skills skills={skills} />)
+    const listItemElements = screen.getAllByRole('listitem')
+    expect(listItemElements).toHaveLength(skills.length)
+  })
+
+  test('renders Login button', () => {
+    render(<Skills skills={skills} />)
+    const loginButton = screen.getByRole('button', { name: 'Login' })
+    expect(loginButton).toBeInTheDocument()
+  })
+
+  test('Start Learning button is not rendered', () => {
+    render(<Skills skills={skills} />)
+    const startLearningButton = screen.queryByRole('button', {
+      name: 'Start learning',
+    })
+    expect(startLearningButton).not.toBeInTheDocument()
+  })
+
+
+  // queryBy ensures the element is not present in document, returns null if no matching element is found
+  test('Start Learning button is eventually displayed', () => {
+    render(<Skills skills={skills} />)
+    const startLearningButton = screen.queryByRole(
+      'button',
+      {
+        name: 'Start learning',
+      }
+    )
+    expect(startLearningButton).not.toBeInTheDocument()
+  })
+})
